28952a0b5df1a7f980a6cf053fd1b684fc3b8353
xieyizhi <594xieyizhi@gmail.com>
2021-06-30 18:24:27 +0800

    quilldata修改

--- lib/src/utils/quill_data.dart
+++ lib/src/utils/quill_data.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class QuillData {
-  static const chatFontSizeScale = 1.3;
-  static const chatFontStyle = TextStyle(fontSize: 16);
+  static double chatFontSizeScale = 1.3;
+  static TextStyle chatFontStyle = const TextStyle(fontSize: 16);
 
-  static const cursorWidth = 2.0;
-  static final double cursorHeight =
+  static double cursorWidth = 2;
+  static double cursorHeight =
       (chatFontStyle.fontSize! * chatFontSizeScale).round().toDouble();
 
   static bool Function(String type)? onInlineEmbedTap;
